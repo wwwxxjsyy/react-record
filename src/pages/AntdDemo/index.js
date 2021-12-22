9366461ab2656b334684a440b196f2d011404b14
songhaoran <songhaoran@ikcrm.com>
2021-12-22 10:09:12 +0800

    提交

--- src/pages/AntdDemo/index.js
+++ src/pages/AntdDemo/index.js
@@ -1,9 +1,13 @@
-import React, { } from "react";
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React, { useEffect, } from "react";
 
 const AntdDemo = () => {
+  useEffect(() => {
+
+  }, []);
   return (
     <div>
-      123
+
     </div>
   );
 };
